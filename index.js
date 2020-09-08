07de825df80cac04067017a66429c81d660df009
Paul von Hagen <pvonhagen26@gmail.com>
2020-09-08 02:54:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,15 @@ class Polygon{
   }
 
 }
+
+class Triangle extends Polygon{
+
+  get isValid(){
+    if (this.count !== 3) return;
+    let side1 = this.sides[0]
+    let side2 = this.sides[1]
+    let side3 = this.sides[2]
+    return ((side1 + side2 > side3) && (side1 + side3 > side2) && (side2 + side3 > side1))
+  }
+  }
+}
